cac9b8040b64ad8e57a9059888f5d42a94460896
currysaucee <jonathanau1999@gmail.com>
2025-01-10 10:32:06 +0800

    Update __init__.py
    - Missed out from previous commit

--- dbs_backend_flask/app/__init__.py
+++ dbs_backend_flask/app/__init__.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+
+from .routes.websocket import init_socketio
 from .config import Config
 from flask_cors import CORS
 
 db = SQLAlchemy()
 jwt = JWTManager()
+
+@jwt.unauthorized_loader
+def unauthorized_callback(callback):
+    return jsonify({"error": "Missing or invalid token"}), 401
 
 def create_app():
     app = Flask(__name__)
@@ -18,8 +24,14 @@ def create_app():
     jwt.init_app(app)  # Initialize JWT
 
     with app.app_context():
-        from .routes import bp
-        app.register_blueprint(bp)  # Register routes
+        from .routes.auth_routes import bp as auth_routes
+        from .routes.data_routes import bp as data_routes
+        
+        app.register_blueprint(auth_routes, url_prefix="/auth")
+        app.register_blueprint(data_routes, url_prefix="/data")  # Register data_routes
         db.create_all()  # Create database tables
+        
+        init_socketio(app)
+
 
     return app
