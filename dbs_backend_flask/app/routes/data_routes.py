89ab8551e10e4588c6998db3b89f3f22f3feaded
lionelgwk <lionelgwk@icloud.com>
2025-01-11 14:10:45 +0800

    chore: deleted comments

--- dbs_backend_flask/app/routes/data_routes.py
+++ dbs_backend_flask/app/routes/data_routes.py
@@ -121,21 +121,6 @@ def create_order():
 
 
 
-# id = db.Column(db.Integer, primary_key=True)
-#     requestorId = db.Column(db.Integer, db.ForeignKey('accounts.id'), nullable=False)
-#     receiverId = db.Column(db.Integer, db.ForeignKey('accounts.id'), nullable=False)
-#     alertId = db.Column(db.Integer, db.ForeignKey('alerts.id'), nullable=True)
-#     carbonUnitPrice = db.Column(db.Float, nullable=False)
-#     carbonQuantity = db.Column(db.Float, nullable=False)
-#     status = db.Column(db.String(120), nullable=False)
-#     createdAt = db.Column(db.DateTime, nullable=False)
-#     updatedAt = db.Column(db.DateTime, nullable=False)
-#     requestType = db.Column(db.String(120), nullable=False)
-#     requestReason = db.Column(db.String(120), nullable=True)
-#     rejectReason = db.Column(db.String(120), nullable=True)
-
-
-
         
 
 
