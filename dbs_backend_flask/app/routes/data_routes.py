9b5ecabb0031442bd77b620ede50340b500fe705
lionelgwk <lionelgwk@icloud.com>
2025-01-11 14:21:46 +0800

    chore: shifted to order_routes

--- dbs_backend_flask/app/routes/data_routes.py
+++ dbs_backend_flask/app/routes/data_routes.py
@@ -74,69 +74,6 @@ def delete_account():
     except:
         return jsonify({'error': 'There was an issue deleting the user.'}), 400
     
-@bp.route("/get_all_orders", methods=["GET"])
-def get_all_orders():
-    orders = Order.query.all()
-    return jsonify(orders), 200
-
-@bp.route("/get_order_by_id/<string:id>", methods=["GET"])
-def get_order_by_id(id):
-    order = Order.query.filter_by(id=id).first()
-    if not order:
-        return jsonify({"error": "Order not found"}), 404
-    return jsonify(order.serialize()), 200
-
-@bp.route("/create_order", methods=["POST"])
-def create_order():
-    data = request.json
-
-    try:
-        new_alert = Alert(
-            accountId = data.get("receiverId"),
-            alertDateTime = datetime.now() + timedelta(days=10),
-            alertText = f"You have yet to approve {data.get("requestorId")}'s request to {data.get("requestType")} {data.get("carbon_quantity")} units of carbon.",
-            alertStatus = "Scheduled"
-        )
-        
-        new_order = Order(
-            requestor_id = data.get("requestorId"),
-            receiver_id = data.get("receiverId"),
-            alert_id = new_alert.id,
-            carbonQuantity = data.get("carbonQuantity"),
-            status = "Active",
-            createdAt = datetime.now(),
-            updatedAt = None,
-            requestType = data.get("requestType"),
-            requestReason = data.get("requestReason"),
-            rejectReason = data.get("rejectReason")
-        )
-
-        db.session.add(new_order)
-        db.session.commit()
-
-        return jsonify("Order successfully created."), 200
-    
-    except:
-        return jsonify("There was a problem creating the order."), 400
-
-
-
-        
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
 @bp.route("/account", methods=["GET"])
 def get_account():
     account = Account.query.filter_by(email=request.args.get("email")).first()
--- dbs_backend_flask/app/routes/order_routes.py
+++ dbs_backend_flask/app/routes/order_routes.py
@@ -1,23 +1,52 @@
 from flask import Blueprint, request, jsonify
 from ..models.Order import Order  
+from ..models.Alert import Alert
 from .. import db
+from datetime import datetime, timedelta
 
 bp = Blueprint("orders_routes", __name__)
 
-bp.route("/order", methods=["POST"])
-def make_order():
+@bp.route("/get_all_orders", methods=["GET"])
+def get_all_orders():
+    orders = Order.query.all()
+    return jsonify(orders), 200
+
+@bp.route("/get_order_by_id/<string:id>", methods=["GET"])
+def get_order_by_id(id):
+    order = Order.query.filter_by(id=id).first()
+    if not order:
+        return jsonify({"error": "Order not found"}), 404
+    return jsonify(order.serialize()), 200
+
+@bp.route("/create_order", methods=["POST"])
+def create_order():
     data = request.json
-    companyID = data.get("companyID")
-    activeAccount = data.get("activeAccount")
-    cashBalance = data.get("cashBalance")
-    carbonBalance = data.get("carbonBalance")
-    
-    if not companyID:
-        return jsonify({"error": "No valid company"}), 400
-    elif activeAccount < 1 or cashBalance < 1 or carbonBalance < 1:
-        return jsonify({"error": "No active account"}), 400
-    
+
     try:
-        order = Order()
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        new_alert = Alert(
+            accountId = data.get("receiverId"),
+            alertDateTime = datetime.now() + timedelta(days=10),
+            alertText = f"You have yet to approve {data.get("requestorId")}'s request to {data.get("requestType")} {data.get("carbon_quantity")} units of carbon.",
+            alertStatus = "Scheduled"
+        )
+        
+        new_order = Order(
+            requestor_id = data.get("requestorId"),
+            receiver_id = data.get("receiverId"),
+            alert_id = new_alert.id,
+            carbonQuantity = data.get("carbonQuantity"),
+            status = "Active",
+            createdAt = datetime.now(),
+            updatedAt = None,
+            requestType = data.get("requestType"),
+            requestReason = data.get("requestReason"),
+            rejectReason = data.get("rejectReason")
+        )
+
+        db.session.add(new_order)
+        db.session.commit()
+
+        return jsonify("Order successfully created."), 200
+    
+    except:
+        return jsonify("There was a problem creating the order."), 400
